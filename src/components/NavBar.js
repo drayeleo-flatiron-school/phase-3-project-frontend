80f63fefc767194c0d785fed7c78f5f132b0cbae
Mariko Janes <Marikojanes@gmail.com>
2022-06-01 17:42:07 -0600

    Added Navbar
    Nav bar with searchbar included.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,5 +1,18 @@
+import React from 'react';
+import { slide as Menu } from 'react-burger-menu'
+
+
+
+
 function NavBar() {
-  return <div>NavBar</div>;
+  // return (
+    // <Menu style={styles}>
+    //     <a id="home" className="menu-item" href="/">Home</a>
+    //     <a id="about" className="menu-item" href="/about">About</a>
+    //     <a id="contact" className="menu-item" href="/contact">Contact</a>
+    //     {/* <a onClick={ this.showSettings } className="menu-item--small" href="">Settings</a> */}
+    // </Menu>
+  // );
 }
 
 export default NavBar;
